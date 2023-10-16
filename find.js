561b236c8b1ad525d74ff3b2baf08e5d3b9b5f18
parthasarathiramasamy <136109699+parthasarathiramasamy@users.noreply.github.com>
2023-10-16 21:31:26 +0530

    Update find.js

--- find.js
+++ find.js
@@ -13,6 +13,15 @@ function find(id){
            document.getElementById('tax_date').value = value.tax_date
            document.getElementById('polu_date').value = value.polu_date
            document.getElementById('permit_date').value = value.permit_date
+           document.getElementById('oil_km').value = value.oil_km 
+           document.getElementById('grease_km').value = value.grease_km
+           document.getElementById('redi_km').value = value.redi_km
+           document.getElementById('crown_km').value = value.crown_km 
+           document.getElementById('gear_km').value = value.gear_km
+           document.getElementById('f1_km').value = value.f1_km
+           document.getElementById('f2_km').value = value.f2_km
+           document.getElementById('r1_km').value = value.r1_km
+           document.getElementById('r2_km').value = value.r2_km
         }
     })
 }
