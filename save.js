0282de0aa24c59e28f28994713666e9c59cf2357
parthasarathiramasamy <136109699+parthasarathiramasamy@users.noreply.github.com>
2023-10-16 21:31:55 +0530

    Update save.js

--- save.js
+++ save.js
@@ -19,7 +19,16 @@ function save(){
                 value.insu_date   = document.getElementById('insu_date').value, 
                 value.tax_date     = document.getElementById('tax_date').value,
                 value.polu_date     = document.getElementById('polu_date').value,
-                value.permit_date     = document.getElementById('permit_date').value
+                value.permit_date     = document.getElementById('permit_date').value,
+                value.oil_km = document.getElementById('oil_km').value, 
+                value.grease_km = document.getElementById('grease_km ').value, 
+                value.redi_km =  document.getElementById('redi_km ').value ,
+                value.crown_km  = document.getElementById('crown_km ').value, 
+                value.gear_km =  document.getElementById('gear_km').value,
+                value.f1_km =  document.getElementById('f1_km').value,
+                value.f2_km = document.getElementById('f2_km').value, 
+                value.r1_km = document.getElementById('r1_km').value,
+                value.r2_km = document.getElementById('r2_km').value
             }
         });
 
@@ -31,13 +40,22 @@ function save(){
         //save
         //get data from form
         var item = {
-            id        : id + 1, 
-            name      : document.getElementById('name').value, 
-            fc_date       : document.getElementById('fc_date').value, 
-            insu_date   : document.getElementById('insu_date').value, 
-            tax_date     : document.getElementById('tax_date').value,
-            polu_date     : document.getElementById('polu_date').value,
-            permit_date     : document.getElementById('permit_date').value
+            id         : id + 1, 
+            name       : document.getElementById('name').value, 
+            fc_date    : document.getElementById('fc_date').value, 
+            insu_date  : document.getElementById('insu_date').value, 
+            tax_date   : document.getElementById('tax_date').value,
+            polu_date  : document.getElementById('polu_date').value,
+            permit_date: document.getElementById('permit_date').value,
+            oil_km     : document.getElementById('oil_km').value,
+            grease_km  : document.getElementById('grease_km').value,
+            redi_km    : document.getElementById('redi_km').value,
+            crown_km   : document.getElementById('crown_km').value,
+            gear_km    : document.getElementById('gear_km').value,
+            f1_km      : document.getElementById('f1_km').value,
+            f2_km      : document.getElementById('f2_km').value,
+            r1_km      : document.getElementById('r1_km').value,
+            r2_km      : document.getElementById('r2_km').value
         }
 
         //add item data to array contactlist
